592df0a18361fd49c6a98291d236e2569d24ca28
rluteijn0253 <rluteijn0253@student.landstede.nl>
2022-03-14 14:06:26 +0100

    commit

--- resources/views/donate.blade.php
+++ resources/views/donate.blade.php
@@ -1,13 +1,30 @@
 <x-guest-layout>
-    <div class="w-1/4 flex flex-col">
-        <div> {{--paypal--}}
-            <a href="https://paypal.me/rluteijn0253?country.x=NL&locale.x=nl_NL" class="justify-center">Paypal</a>
-        </div>
-        <div> {{--ko-fi--}}
-            <a href="https://ko-fi.com/rubenluteijn" class="justify-center" target="_blank"><img style="border:0px;height:28px;" src="https://uploads-ssl.webflow.com/5c14e387dab576fe667689cf/5cbed8a433a3f45a772abaf5_SupportMe_blue.png" border="0" alt="Support Me on Ko-fi"></a>
-        </div>
-        <div class="justify-center">
-            More added soon.
+    <div class="flex flex-col h-96 my-auto items-center justify-center ...">
+        <div class="w-1/4 flex flex-col justify-between text-center ...">
+            <div> {{--paypal--}}
+                <a href="https://paypal.me/rluteijn0253?country.x=NL&locale.x=nl_NL" class="justify-center">
+                    <button class="rounded-full ... bg-teal-400 h-10 w-48 text-white
+                                   hover:bg-zinc-800 hover:border-4 hover:border-teal-200
+                                   active:bg-zinc-800 active:outline-teal-200 mb-5 ">Paypal
+                    </button></a>
+            </div>
+            <div> {{--ko-fi--}}
+                <a href="https://ko-fi.com/rubenluteijn">
+                    <button class="rounded-full ... bg-teal-400 h-10 w-48 text-white
+                                   hover:bg-zinc-800 hover:border-4 hover:border-teal-200
+                                   active:bg-zinc-800 active:outline-teal-200 mb-5 ">Ko-Fi
+                    </button></a>
+            </div>
+            <div>{{--paypal--}}
+                <a href="https://www.patreon.com/rluteijn0253" class="justify-center">
+                    <button class="rounded-full ... bg-teal-400 h-10 w-48 text-white
+                                   hover:bg-zinc-800 hover:border-4 hover:border-teal-200
+                                   active:bg-zinc-800 active:outline-teal-200 mb-5 ">Paypal
+                    </button></a>
+            </div>
+            <div class="justify-center">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus excepturi laborum voluptatibus. Autem doloribus eveniet iste perspiciatis, porro provident quos similique voluptate! Alias animi cumque dolor laborum, modi pariatur repudiandae saepe sint tempore. Asperiores assumenda, aut cupiditate deserunt doloribus, impedit, ipsa iste iusto laudantium molestias nihil placeat repudiandae tempora tenetur!
+            </div>
         </div>
     </div>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,11 +8,11 @@
     <title>Portfolio</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body class="bg-zinc-800 text-white">
+<body class="flex flex-col bg-zinc-800 text-white h-screen">
     <header class="flex flex-row justify-between h-16 bg-zinc-900 content-center container px-40">
         <div>[logo]</div>
         <ul class="flex justify-between w-3/5 mr-2">
-            <a href="projects" class="self-center.">
+            <a href="projects" class="self-center">
                 <button class="rounded-full ... bg-teal-400 h-10 w-48 text-white
                           hover:bg-zinc-800 hover:border-4 hover:border-teal-200
                           active:bg-zinc-800 active:outline-teal-200">Projecten
@@ -32,10 +32,10 @@
             </a>
         </ul>
     </header>
-    <div>
+    <div class="flex-grow">
         {{ $slot }}
     </div>
-    <footer class="text-center">
+    <footer class="text-center flex flex-col h-full justify-end">
         <div class="text-base"> Copyright © 2022 Ruben luteijn. All Rights Reserved</div><br>
         <a href="donate" class="self-center">
             <button class="rounded-full ... bg-teal-400 h-7 w-40 text-white text-s
