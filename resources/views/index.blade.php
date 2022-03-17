b16305f3d8feb01dbc2901228b9a7e8bcd20de0d
rluteijn0253 <rluteijn0253@student.landstede.nl>
2022-03-17 23:37:36 +0100

    commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,31 @@
 <x-guest-layout>
-<div>
-    <h1 class="text-7xl mt-5" style="font-family: 'Monoton', cursive;">
+<section>
+    <h1 class="text-7xl mt-8 flex justify-center" style="font-family: 'Monoton', cursive;">
         Welkom
     </h1>
-</div>
-
-
+    <div class="flex flex-col self-center items-center mt-8">
+        <h1 class="self-center ">
+            Hallo, ik ben Ruben Luteijn.
+        </h1>
+        <p class="w-1/3 text-center">
+            Ik ben <?php
+            //date in mm/dd/yyyy format; or it can be in other formats as well
+            $birthDate = "4/19/2004";
+            //explode the date to get month, day and year
+            $birthDate = explode("/", $birthDate);
+            //get age from date or birthdate
+            $age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
+                ? ((date("Y") - $birthDate[2]) - 1)
+                : (date("Y") - $birthDate[2]));
+            echo $age . " jaar oud";
+            ?>
+            en kom uit Nijkerk. Ik heb vanaf 2016 tot 2021 een VMBO-opleiding gevolgd op het Corlaer College.
+            Momenteel doe ik de opleiding Software-Development op Landstede MBO.
+            Sinds ik met mijn opleiding ben begonnen heb ik al veel kennis opgedaan.
+            Ik heb leren programmeren met verschillende programmeertalen/frameworks.
+            Waaronder: HTML, CSS, JavaScript, PHP, Laravel, Bootstrap, Tailwind CSS enSQL.
+            Verdere hobbies van mij zijn gamen.
+        </p>
+    </div>
+</section>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,11 +10,12 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Monoton&family=Rubik&display=swap" rel="stylesheet">
+    <link rel="icon" href="images/icon.png">
 </head>
 <body class="flex flex-col bg-zinc-800 text-white h-screen font-sans" style="font-family: 'Rubik', sans-serif;">
     <header class="flex flex-row justify-between py-3.5 bg-zinc-900 content-center container px-40">
-        <a href="/"><img src="">[logo]</a>
-        <ul class="flex justify-between w-3/5 mr-2 flex md:flex-0">
+        <a href="/"><img class="w-14 h-auto" src="images/logo.png"></a>
+        <ul class="flex justify-between w-3/5 flex md:flex-0">
             <a href="projects" class="self-center">
                 <button class="rounded-full ... bg-teal-400 h-10 w-48 text-white
                           hover:bg-zinc-800 hover:border-4 hover:border-teal-200
@@ -39,7 +40,10 @@
         {{ $slot }}
     </div>
     <footer class="text-center flex flex-col h-full justify-end">
-        <div class="text-base"> Copyright © 2022 Ruben luteijn. All Rights Reserved</div>
+        <div class="text-base"> Copyright ©<?php $copyYear = 2004;
+                                                 $curYear = date('Y');
+                                                 echo $copyYear . (($copyYear != $curYear) ? '-' . $curYear : '');
+                                                 ?> Ruben luteijn. All Rights Reserved</div>
         <a href="donate" class="self-center">
             <button class="rounded-full ... bg-teal-400 h-8 w-40 text-white text-s
                           hover:bg-teal-600 mb-4">Donate
