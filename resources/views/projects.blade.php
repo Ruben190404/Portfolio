30db4375722435c848b37701a153a14c3b4aa00a
rluteijn0253 <rluteijn0253@student.landstede.nl>
2022-03-21 10:40:58 +0100

    commit

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -2,10 +2,13 @@
     <h1 class="text-7xl flex justify-center mt-6" style="font-family: 'Monoton', cursive;">
         Projecten
     </h1>
+    <section>
+
+    </section>
     <section class="w-4/5 h-96 flex ml-auto mt-8">
         @foreach($projects AS $project)
             <div class="bg-zinc-700 rounded-3xl w-1/4 flex flex-col items-center mr-4">
-                <img src="images/projectsHP/{{$project->picture}}" class="rounded-3xl w-4/5 h-52">
+                <img src="images/projectsHP/{{$project->picture}}" class="rounded-3xl w-4/5 mt-4 h-52">
                 <div class="text-lg font-medium">
                     {{$project->title}}
                 </div>
