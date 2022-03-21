e2b6653b2a5d7e92ff219c4dfb756e7e1826fd5b
rluteijn0253 <rluteijn0253@student.landstede.nl>
2022-03-21 08:20:23 +0100

    commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,31 +1,33 @@
 <x-guest-layout>
-<section class="bg-zinc-700 w-2/5 h-auto p-16 rounded-3xl items-center">
-    <h1 class="text-7xl flex justify-center" style="font-family: 'Monoton', cursive;">
-        Welkom
-    </h1>
-    <div class="flex flex-col self-center items-center mt-8">
-        <h1 class="self-center ">
-            Hallo, ik ben Ruben Luteijn.
-        </h1>
-        <p class="text-center">
-            Ik ben <?php
-            //date in mm/dd/yyyy format; or it can be in other formats as well
-            $birthDate = "4/19/2004";
-            //explode the date to get month, day and year
-            $birthDate = explode("/", $birthDate);
-            //get age from date or birthdate
-            $age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
-                ? ((date("Y") - $birthDate[2]) - 1)
-                : (date("Y") - $birthDate[2]));
-            echo $age . " jaar oud";
-            ?>
-            en kom uit Nijkerk. Ik heb vanaf 2016 tot 2021 een VMBO-opleiding gevolgd op het Corlaer College.
-            Momenteel doe ik de opleiding Software-Development op Landstede MBO.
-            Sinds ik met mijn opleiding ben begonnen heb ik al veel kennis opgedaan.
-            Ik heb leren programmeren met verschillende programmeertalen/frameworks.
-            Waaronder: HTML, CSS, JavaScript, PHP, Laravel, Bootstrap, Tailwind CSS enSQL.
-            Verdere hobbies van mij zijn gamen.
-        </p>
+    <div class="flex justify-center mt-8">
+        <section class="bg-zinc-700 w-2/5 h-auto p-16 rounded-3xl">
+            <h1 class="text-7xl flex justify-center" style="font-family: 'Monoton', cursive;">
+                Welkom
+            </h1>
+            <div class="flex flex-col self-center items-center mt-8">
+                <h1 class="self-center ">
+                    Hallo, ik ben Ruben Luteijn.
+                </h1>
+                <p class="text-center">
+                    Ik ben <?php
+                    //date in mm/dd/yyyy format; or it can be in other formats as well
+                    $birthDate = "4/19/2004";
+                    //explode the date to get month, day and year
+                    $birthDate = explode("/", $birthDate);
+                    //get age from date or birthdate
+                    $age = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
+                        ? ((date("Y") - $birthDate[2]) - 1)
+                        : (date("Y") - $birthDate[2]));
+                    echo $age . " jaar oud";
+                    ?>
+                    en kom uit Nijkerk. Ik heb vanaf 2016 tot 2021 een VMBO-opleiding gevolgd op het Corlaer College.
+                    Momenteel doe ik de opleiding Software-Development op Landstede MBO.
+                    Sinds ik met mijn opleiding ben begonnen heb ik al veel kennis opgedaan.
+                    Ik heb leren programmeren met verschillende programmeertalen/frameworks.
+                    Waaronder: HTML, CSS, JavaScript, PHP, Laravel, Bootstrap, Tailwind CSS enSQL.
+                    Verdere hobbies van mij zijn gamen.
+                </p>
+            </div>
+        </section>
     </div>
-</section>
 </x-guest-layout>
