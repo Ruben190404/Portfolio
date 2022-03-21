0715b5e61c00013cd7720aba30cbc814c46adcaf
rluteijn0253 <rluteijn0253@student.landstede.nl>
2022-03-21 13:25:13 +0100

    commit

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,9 +3,13 @@
         Contact
     </h1>
     <section class="bg-zinc-700"> {{--contact info--}}
-
+        <div class="">Contacteer Mij</div> {{--contacteer mij--}}
+        <div class="">Om met mij contact op te nemen gebruik één van deze middelen of vul het formulier in.</div> {{--vul formulier in--}}
+        <div class=""></div> {{--linkedin--}}
+        <div class=""></div> {{--telefoon nummer--}}
+        <div class=""></div> {{--email--}}
     </section>
-    <section class="bg-zinc-700"> {{--contact form--}}
+    <section class="bg-zinc-700"> {{--contact form met naam, email, telefoon nummer en korte tekst--}}
 
     </section>
 </x-guest-layout>
