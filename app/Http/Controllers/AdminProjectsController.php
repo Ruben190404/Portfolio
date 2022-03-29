31c6f09f2b2ae77d1e9c91b34ca0686f9b3453bb
rluteijn0253 <rluteijn0253@student.landstede.nl>
2022-03-29 09:42:46 +0200

    commit

--- app/Http/Controllers/AdminProjectsController.php
+++ app/Http/Controllers/AdminProjectsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project_language;
 use App\Models\Language;
 use App\Models\Project;
 use Illuminate\Http\Request;
@@ -28,12 +29,24 @@ class AdminProjectsController extends Controller
      ]);
     }
 
-    public function update(Project $project, Request $request) {
+    public function update(Project $project, Project_language $project_language,Request $request) {
         $project->title = $request->input('title');
         $project->description = $request->input('description');
-        $project->picture = $request->input('image');
-//        $project->languages = $request->input('language');
+
+        if($request->hasFile('image')) {
+            $image = $request->file('image');
+            $filename = $image->getClientOriginalName();
+            $image->move(public_path('images/services'), $filename);
+            $project->picture = $request->file('image')->getClientOriginalName();
+        }
+
+        $project_language->project_id = $request->input('id');
+        $project_language->language_id = $request->input('language');
+
+
+        $project_language->save();
         $project->save();
         return redirect()->route('admin.projects.list')->with('status', 'Project updated successfully');
+
     }
 }
--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -9,8 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <form action="" method="post">
+                    <form action="" method="post" enctype="multipart/form-data">
                         @csrf
+                        <input type="hidden" name="id" value="{{ $project->id }}">
                         <input type="text" name="title" placeholder="Titel" value="{{ $project->title }}">
                         <input type="text" name="description" placeholder="Beschrijving" value="{{ $project->description }}">
                         <img src="{{ asset('images/projectsHP/' . $project->picture) }}" alt="" class="w-96 h-auto">
