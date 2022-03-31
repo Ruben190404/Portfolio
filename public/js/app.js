3b731e098a21c528412927f09624a3d33b2b8fca
rluteijn0253 <rluteijn0253@student.landstede.nl>
2022-03-31 13:39:26 +0200

    Portfolio 1.0

--- public/js/app.js
+++ public/js/app.js
@@ -5523,6 +5523,15 @@ __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
 window.Alpine = alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"];
 alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"].start();
+var clicked = false;
+document.getElementById('togglemenu').addEventListener('click', function () {
+  if (clicked === false || document.getElementById("nav").style.display === "none") {
+    document.getElementById("nav").style.display = 'flex';
+    clicked = true;
+  } else {
+    document.getElementById("nav").style.display = "none";
+  }
+});
 
 /***/ }),
 
